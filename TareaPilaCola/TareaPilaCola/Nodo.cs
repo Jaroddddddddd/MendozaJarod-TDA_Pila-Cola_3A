6dfefb862566db11131e2808794b62836e5fbf6f
Jaroddddddddd <56411273+Jaroddddddddd@users.noreply.github.com>
2019-11-06 23:07:20 -0500

    Add files via upload

---
+++ TareaPilaCola/TareaPilaCola/Nodo.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TareaPilaCola
+{
+    class Nodo
+    {
+        int dato;
+        Nodo frente;
+        Nodo siguiente;
+        Nodo cima;
+
+        public Nodo(int dato)
+        {
+            this.dato = dato;
+            this.siguiente = null;
+        }
+
+        public static Nodo Crear(Nodo pilacola)
+        {
+            Console.WriteLine("Ingrese elemento a guardar");
+            int valor = int.Parse(Console.ReadLine());
+            pilacola = new Nodo(valor);
+            pilacola.frente = pilacola.cima = pilacola;
+           
+            return pilacola;
+        }
+        public static Nodo APilarEncolar(Nodo pilacola)
+        {
+
+            if (pilacola == null)
+            {
+                Console.WriteLine("La estructura de datos no ha sido creada, escoja opción 1");
+                return pilacola;
+            }
+
+
+            Console.WriteLine("Ingrese elemento a guardar");
+            int valor = int.Parse(Console.ReadLine());
+            Nodo nodoNuevo = new Nodo(valor);
+
+            Nodo aux = pilacola;
+            for (; aux.siguiente != null; aux = aux.siguiente) ;
+            aux.siguiente = nodoNuevo;
+            pilacola.cima = nodoNuevo;
+
+            return pilacola;
+        }
+        public static Nodo DesapilarDesencolar(Nodo pilacola, int opciongenaral)
+        {
+            if (pilacola == null)
+            {
+                Console.WriteLine("La estructura de datos no ha sido creada... escoja opción 1");
+                return pilacola;
+            }
+            if (pilacola.siguiente == null)
+            {
+                Console.WriteLine("Elemento: " + pilacola.dato);
+                return pilacola.siguiente;
+            }
+
+            if (opciongenaral == 2)
+            {
+                if (pilacola == null)
+                {
+                    Console.WriteLine("La estructura de datos no ha sido creada... escoja opción 1");
+                    return pilacola;
+                }
+
+                Console.WriteLine("Elemento: " + pilacola.dato);
+                pilacola = pilacola.siguiente;
+
+                return pilacola;
+
+
+            }
+            else
+            {
+
+                Nodo aux = pilacola;
+                for (; aux.siguiente.siguiente != null; aux = aux.siguiente) ;
+                Console.WriteLine("Elemento: " + aux.siguiente.dato);
+                aux.siguiente = aux.siguiente.siguiente;
+                pilacola.cima = aux;
+                return pilacola;
+
+            }
+
+        }
+
+        public static void Imprimir(Nodo miPila)
+        {
+            if (miPila == null)
+                Console.WriteLine("La estructura de datos no ha sido creada... escoja opción 1");
+            for (Nodo aux = miPila; aux != null; aux = aux.siguiente)
+                Console.WriteLine("Dato:" + aux.dato + "\n");
+        }
+
+        public static void ImprimirCima(Nodo miPila)
+        {
+            if (miPila == null)
+                Console.WriteLine("La estructura de datos no ha sido creada... escoja opción 1");
+            else
+
+                Console.WriteLine("Cima: " + miPila.cima.dato + "\n");
+
+
+        }
+
+
+        public static void estaVacia(Nodo miPila)
+        {
+            if (miPila == null)
+                Console.WriteLine("La estructura de datos esta vacia");
+            else
+                Console.WriteLine("La estructura de datos no esta vacia");
+        }
+    }
+}
---
+++ TareaPilaCola/TareaPilaCola/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace TareaPilaCola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int opcion = 0;
+
+            Nodo pilacola = null;
+
+            Console.WriteLine("Escoja La estructura de datos a usar\n"
+                        + "(1) Pila  \n"
+                        + "(2) Cola \n");
+            int opgeneral = int.Parse(Console.ReadLine());
+
+            do
+            {
+                Console.WriteLine("Ingrese opción \n" + 
+                    "(1) Crear  \n" + 
+                    "(2) Apilar/Encolar \n" + 
+                    "(3) Desapilar/Desencolar\n" + 
+                    "(4) Cima\n" +
+                    "(5) Preguntar si la estructura de datos esta vacia \n" +
+                    "(6) Imprimir Toda la estructura de datos \n" + 
+                    "(7) Salir \n");
+                opcion = int.Parse(Console.ReadLine());
+
+                switch (opcion)
+                {
+                    case 1:
+                        pilacola = Nodo.Crear(pilacola);
+                        break;
+                    case 2:
+                        pilacola = Nodo.APilarEncolar(pilacola);
+                        break;
+                    case 3:
+                        pilacola = Nodo.DesapilarDesencolar(pilacola, opgeneral);
+                        break;
+                    case 4:
+                        Nodo.ImprimirCima(pilacola);
+                        break;
+                    case 5:
+                        Nodo.estaVacia(pilacola);
+                        break;
+                    case 6:
+                        Nodo.Imprimir(pilacola);
+                        break;
+
+                }
+            } while (opcion != 7);
+
+        }
+    }
+    }
